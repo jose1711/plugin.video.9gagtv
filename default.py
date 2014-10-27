70d59ef6d759c353e97d38bf19bf30b49eafb359
bromix <bromix@gmx.net>
2014-10-27 18:44:59 +0100

    ADD: initial add

---
+++ default.py
@@ -0,0 +1,5 @@
+from resources.lib import kodimon
+from resources.lib import nine_gag
+
+__provider__ = nine_gag.Provider()
+kodimon.run(__provider__)
---
+++ resources/lib/nine_gag/provider.py
@@ -0,0 +1,11 @@
+__author__ = 'bromix'
+
+
+from resources.lib import kodimon
+
+class Provider(kodimon.AbstractProvider):
+    def __init__(self, plugin=None):
+        kodimon.AbstractProvider.__init__(self, plugin)
+        pass
+
+    pass
